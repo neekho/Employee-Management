428df8fc4c3c103e44a52296ebb69e6eb17b373a
Nico <remullanico28@gmail.com>
2024-03-10 23:47:14 +0800

    employee deletion progress 1

--- frontend/src/components/EmployeeCard/EmployeeCard.jsx
+++ frontend/src/components/EmployeeCard/EmployeeCard.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const handleDelete = async (id) => {
+  if (window.confirm(`Are you sure you want to delete this employee? ${id}`)) {
+    console.log("pressed");
+  }
+};
+
 const EmployeeCard = ({
   _id,
   firstName,
@@ -28,7 +34,11 @@ const EmployeeCard = ({
           <Link className="bg-yellow-500 p-3 rounded-md" to={`/edit/${_id}`}>
             Edit
           </Link>
-          <Link className="bg-red-500 p-3 rounded-md" to={`/delete/${_id}`}>
+          <Link
+            className="bg-red-500 p-3 rounded-md"
+            to={`/delete/${_id}`}
+            onClick={() => handleDelete(firstName)}
+          >
             Delete
           </Link>
         </td>
